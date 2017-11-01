2e47a6b7afaf9f340ca729d174b0b0abb7de3201
Nick Nauert <nicknauert@gmail.com>
2017-11-01 15:26:15 -0400

    refresh

--- main.js
+++ main.js
@@ -53,3 +53,33 @@ contactClose.addEventListener('click', function(e){
   contact.classList.remove('showPage');
   contact.classList.add('hidePage');
 })
+
+///////////////////////
+// BG PARALLAX
+
+let lFollowX = 0,
+lFollowY = 0,
+x = 0,
+y = 0
+const friction = 1 / 40;
+const bg = document.querySelector(".bg")
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  bg.style.transform = translate
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+window.addEventListener('mousemove', function(e){
+  var lMouseX = Math.max(-100, Math.min(100, window.innerWidth / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, window.innerHeight / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+})
+
+moveBackground();
